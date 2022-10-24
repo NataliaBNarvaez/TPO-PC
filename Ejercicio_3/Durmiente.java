8ab23968acb0bf6f6442923d12b3cfee8c22ec9a
Natalia N <102324463+NataliaBNarvaez@users.noreply.github.com>
2022-10-24 13:42:25 -0300

    Ejercicio 03

---
+++ Ejercicio_3/Durmiente.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TPO;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Durmiente implements Runnable {
+
+    Reloj reloj;
+    int horaLevantarse;
+    boolean fueATrabajar = false;
+
+    public Durmiente(Reloj reloj, int hora) {
+        this.horaLevantarse = hora;
+        this.reloj = reloj;
+    }
+
+    int n = 0;
+
+    public void run() {
+        while (reloj.getN() < 15) {
+            reloj.despertarVecino();
+            fueATrabajar = reloj.irATrabajar(horaLevantarse);
+            if (fueATrabajar) {
+                reloj.volverDeTrabajar();
+                reloj.aumentarN();
+            }
+        }
+
+    }
+
+}
